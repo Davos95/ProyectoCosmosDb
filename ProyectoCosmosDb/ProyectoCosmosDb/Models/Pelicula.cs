c2f534a5b8bd643879a55a6b7c0e286701854567
David <davidvalencia_@hotmail.com>
2019-03-25 14:04:47 +0100

    Version 0.5

---
+++ ProyectoCosmosDb/ProyectoCosmosDb/Models/Pelicula.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoCosmosDb.Models
+{
+    public class Pelicula
+    {
+        [JsonProperty(PropertyName = "id")]
+        public String Id { get; set; }
+        public String Titulo { get; set; }
+        public String TituloOriginal { get; set; }
+        public String Sinopsis { get; set; }
+        public String Genero { get; set; }
+        public String Imagen { get; set; }
+
+    }
+}
---
+++ ProyectoCosmosDb/ProyectoCosmosDb/Repositories/RepositoryCosmosDb.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using ProyectoCosmosDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoCosmosDb.Repositories
+{
+    public class RepositoryCosmosDb
+    {
+        IConfiguration configuration;
+        String bbdd;
+        String collection;
+        DocumentClient client;
+
+        public RepositoryCosmosDb(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.bbdd = "PeliculasBBDD";
+            this.collection = "PeliculasCollection";
+            String endpoint = configuration["CosmosDb:endpoint"];
+            String primarykey = configuration["CosmosDb:primarykey"];
+            this.client = new DocumentClient(new Uri(endpoint), primarykey);
+        }
+        public async Task CrearBBDD()
+        {
+            Database bbdd = new Database() { Id = this.bbdd };
+            await this.client.CreateDatabaseAsync(bbdd);
+        }
+
+        public async Task CrearColeccionPeliculas()
+        {
+            DocumentCollection collection = new DocumentCollection() { Id = this.collection };
+            //Crea la URI de la base de datos para implementar la colleción
+            await this.client.CreateDocumentCollectionAsync(UriFactory.CreateDatabaseUri(this.bbdd), collection);
+        }
+
+        public async Task InsertarPelicula(Pelicula peli)
+        {
+            await this.client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(this.bbdd, this.collection), peli);
+        }
+
+        public async Task ModificarPelicula(Pelicula peli)
+        {
+            Uri uri = UriFactory.CreateDocumentUri(this.bbdd, this.collection, peli.Id);
+            await this.client.ReplaceDocumentAsync(uri, peli);
+
+        }
+
+        public async Task EliminarPelicula(String idPelicula)
+        {
+            Uri uri = UriFactory.CreateDocumentUri(this.bbdd, this.collection, idPelicula);
+            await this.client.DeleteDocumentAsync(uri);
+        }
+
+        public List<Pelicula> BuscarPeliculasGenero(String genero)
+        {
+            //Indicamos que nos devuelvan todos los datos
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 };
+
+            IQueryable<Pelicula> query = this.client.CreateDocumentQuery<Pelicula>(
+                    UriFactory.CreateDocumentCollectionUri(this.bbdd, this.collection), queryOptions)
+                    .Where(x => x.Genero == genero);
+            return query.ToList();
+        }
+
+    }
+}
